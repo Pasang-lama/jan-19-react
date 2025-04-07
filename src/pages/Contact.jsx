042d92f185a9c241ea130aaf41acb134b2c2d8f1
Pasang-lama <lpasang182@gmail.com>
2025-04-07 08:39:17 +0545

    works on the form handle

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,14 +2,81 @@ import React from 'react'
 import Header from '../component/layouts/Header'
 import Footer from '../component/layouts/Footer'
 
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const contactformschema = yup.object().shape({
+    name: yup.string().required("Full name is required").min(3, "Full name must be at least 3 characters long"),
+   
+   
+    email: yup.string().required("Email is required").email("Invalid email address"),
+    password: yup.string()
+    .required("Password is required")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/,
+      "Password must be at least 8 characters, include uppercase, lowercase, number, and special character"
+    ),
+    comformpassword: yup.string()
+    .required("Repeat password is required")
+    .oneOf([yup.ref("password")], "Passwords must match"),
+    number:yup.string()
+    .required("Phone number is required")
+    .matches(
+        /^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/,
+        "Invalid phone number format"
+    )
+    .max(10, "Phone number must be 10 digits"),
+})
+
 function Contact() {
+
+    const { register, handleSubmit, formState : { errors } } = useForm({
+        resolver: yupResolver(contactformschema),
+    });
+
+    const contactsubmit = (data) => {
+        console.log(data);
+    }
+
     return (
         <>
             <Header />
             <section className="container my-5">
-                <h1 className="text-center">Contact us page</h1>
+                <h1 className="text-center mb-5">Contact us page</h1>
+                <form onSubmit={handleSubmit(contactsubmit)} >
+                    <div className="row gy-4">
+                        <div className="col-lg-6">
+                            <label htmlFor="name" className="form-label">Full Name*</label>
+                            <input type="text" id='name' {...register('name')} className="form-control" name='name' placeholder='Enter your name' />
+                            <span className='text-danger text-itallic'>{errors?.name?.message}</span>
+                        </div>
+                        <div className="col-lg-6">
+                            <label htmlFor="email" className="form-label">Email*</label>
+                            <input type="text" id='email' {...register('email')} className="form-control" name='email' placeholder='Enter your email address' />
+                            <span className='text-danger text-itallic'>{errors?.email?.message}</span>
+                        </div>
+                        <div className="col-lg-6">
+                            <label htmlFor="password" className="form-label">password*</label>
+                            <input type="password" id='password' {...register('password')} className="form-control" name='password' placeholder='Enter your password' />
+                            <span className='text-danger text-itallic'>{errors?.password?.message}</span>
+                        </div>
+                        <div className="col-lg-6">
+                            <label htmlFor="comformpassword" className="form-label">comformpassword*</label>
+                            <input type="password" id='comformpassword' {...register('comformpassword')} className="form-control" name='comformpassword' placeholder='Re-Enter your password' />
+                            <span className='text-danger text-itallic'>{errors?.comformpassword?.message}</span>
+                        </div>
+
+                        <div className="col-lg-6">
+                            <label htmlFor="number" className="form-label">number*</label>
+                            <input type="text" id='number' {...register('number')} className="form-control" name='number' placeholder='Enter your number' />
+                            <span className='text-danger text-itallic'>{errors?.number?.message}</span>
+                        </div>
+                    </div>
+                    <button type='submit' className='btn btn-primary mt-5'>Submit</button>
+                </form>
             </section>
-            
+
             <Footer />
         </>
     )
--- src/pages/Productdetails.jsx
+++ src/pages/Productdetails.jsx
@@ -2,25 +2,36 @@ import React, { use } from 'react'
 import Header from '../component/layouts/Header'
 import Footer from '../component/layouts/Footer'
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
 
 function Productdetails() {
-
+    const { id } = useParams()
     const [product, setProduct] = useState([])
-      useEffect(() => {
-          fetch("https://fakestoreapi.com/products")
-              .then(response => response.json())
-              .then(json => setProduct(json))
-      }, [])
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then(response => response.json())
+            .then(json => setProduct(json))
+    }, [])
+    const result = product.find((item) => item.id == id)
 
-      const result = product.find((item) => item.id === id)
     return (
         <>
             <Header />
             <section className="container my-5">
-                <h1>Product details</h1>
+                <h1 className='bg-success p-2 text-light fs-4 mb-5'>{result?.title}</h1>
                 <div className="row gy-4">
-                    <div className="col-lg-4"></div>
-                    <div className="col-lg-8"></div>
+                    <div className="col-lg-4">
+                        <figure>
+                            <img src={result?.image} alt="" className='img-fluid' />
+                        </figure>
+                    </div>
+                    <div className="col-lg-8">
+                        <span className='badge bg-info'>{result?.category}</span> <span>({result?.rating?.rate})({result?.rating?.count})</span>
+                        <h2 className='fw-bold fs-2'>{result?.title}</h2>
+                        <span className='fw-bold text-uppercase '>USD.${result?.price}</span>
+                        <hr />
+                        <p className='mt-5'>{result?.description}</p>
+                    </div>
                 </div>
             </section>
             <Footer />
